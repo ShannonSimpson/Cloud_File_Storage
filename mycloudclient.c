e607a1038d41ed0c12562dc116ee6672cadc46c1
ShannonSimpson <sfsimp2@cs.uky.edu>
2014-12-07 19:38:49 -0500

    client is sending twice, but sending

--- mycloudclient.c
+++ mycloudclient.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "csapp.h"
 #include <sys/socket.h>
@@ -10,7 +10,7 @@
 #define MAX_FILENAME 80
 #define MAX_SIZE 102400 
 #define GET 0
-#define PUT 1
+#define STORE 1
 #define DELETE 2
 #define LIST 3
 
@@ -18,12 +18,12 @@ typedef struct req_struct {
 	int key;
 	int type;
 	char filename[MAX_FILENAME];
-	char call[MAX_SIZE];
+	char soul[MAX_SIZE];
 	uint32_t size;
 }req;
 
 typedef struct resp_struct {
-	char returns[MAX_SIZE];
+	char soul[MAX_SIZE];
 	uint32_t status;
 	uint32_t size;
 }resp;
@@ -31,6 +31,10 @@ typedef struct resp_struct {
 size_t size_of_req(req* rq)
 {
 	printf("in size of\n");
+	return sizeof(rq)+MAX_FILENAME*sizeof(char)+sizeof(uint32_t)+sizeof(char)*rq->size;
+}
+
+size_t request_size(req* rq) {
 	return sizeof(rq)+MAX_FILENAME*sizeof(char)+sizeof(uint32_t)+sizeof(char)*rq->size;
 }
 
@@ -48,32 +52,29 @@ int send_request(req * rq, char* host, char *port, resp *rp)
 	struct addrinfo *serverInfo;
 	memset(&socketInfo, 0, sizeof(socketInfo));
 
-	socketInfo.ai_family = AF_INET;
+	socketInfo.ai_family = AF_UNSPEC;
 	socketInfo.ai_socktype = SOCK_STREAM;
-	getaddrinfo(host, port, &socketInfo, &serverInfo);
-	if(getaddrinfo(host, port, $socketInfo, &serverInfo)!= 0){
-		printf(stderr, "Hostname failed.\n");
+	if(getaddrinfo(host, port, &socketInfo, &serverInfo)!= 0){
+		fprintf(stderr, "Hostname failed.\n");
 		return -4;
 	}
 	servAddr = serverInfo->ai_addr;
-	printf("here1\n");
-	connect(sock, servAddr, sizeof(*servAddr));
-	if(connect(sock, servaddr, sizeof(*servaddr)) < 0) {
-		printf(stderr, "Server connection failed.\n");
-		return -3;
+//	printf("here1\n");
+	if(connect(sock, servAddr, sizeof(*servAddr)) < 0) {
+		fprintf(stderr, "Server connection failed.\n");
+		return -3;	
 	}
-	printf("here2\n");
+//	printf("here2\n");
 	send(sock, rq, size_of_req(rq), 0);
-	if(read(sock, resp, MAX_SIZE) < 0) {
+	if(read(sock, rp, MAX_SIZE) < 0) {
 		close(sock);
 		return -4;
 	}
-	printf("here3\n");
-	printf("made it here\n");
+//	printf("here3\n");
 
-	handle_response(resp);
+	//handle_response(rp);
 
-	if(rp->status == 1)
+/*	if(rp->status == 1)
 	{
 		close(sock);
 		printf("error\n");
@@ -82,7 +83,7 @@ int send_request(req * rq, char* host, char *port, resp *rp)
 	{
 		printf("%s\n", rp->returns);
 	}
-	close(sock);
+*/	close(sock);
 	freeaddrinfo(serverInfo);
 	printf("request sent successfully\n");
 	return 0;
@@ -96,12 +97,23 @@ int get_rq (char *in)
 		return GET; 	
 	}
 	else if(strncmp(in, "put", 3) == 0)
-	{	return PUT;	}
+	{	return STORE;	}
 	else if(strncmp(in, "del", 3) == 0 || strncmp(in, "rm", 2)== 0)
 	{	return DELETE;	}
 	else if(strncmp(in, "list", 4) == 0)
 	{	return LIST;	}
 	return -1;
+}
+
+
+char* name_rq (int type) {
+	switch(type) {
+		case GET: return "get";
+		case STORE: return "put";
+		case DELETE: return "delete";
+		case LIST: return "list";
+		default: return "other";
+	}
 }
 
 
@@ -130,19 +142,18 @@ int main(int argc, char **argv)
 	
 	//Rio_readinitb(&rio, clientfd);
 
-	switch (req.type) {
-		case REQ_STORE: req.size = fread(req.contents, sizeof(char), CONTENT_MAX, stdin);
-			rq.contents[req.size] = 0;
+	switch (rq.type) {
+		case STORE: rq.size = fread(rq.soul, sizeof(char), MAX_SIZE, stdin);
+			rq.soul[rq.size] = 0;
 			rq.size += 1;
 			break;
-		case REQ_GET: break;
-		case REQ_DELETE: break;
-		case REQ_LIST: break;
+		case GET: break;
+		case DELETE: break;
+		case LIST: break;
 	}
 
-	response resp;
 
-	if (send_request (&rq, host, port, &resp) < 0) {
+	if (send_request (&rq, host, port, &rp) < 0) {
 		exit(-5);
 	
 	}
