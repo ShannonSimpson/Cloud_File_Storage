0eee29123b8df53607836f0b9889a44e69f780ca
ShannonSimpson <sfsimp2@cs.uky.edu>
2014-12-07 20:14:46 -0500

    server

--- mycloudserver.c
+++ mycloudserver.c
@@ -10,17 +10,53 @@
 * file, and list the files stored on the    *
 * server.				    *
 ********************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+#define MAX_PENDING 5
+
+void serve(int, int);
+
 int main(int argc, char **argv) {
 	// create and configure the listening socket
-	int listenfd, connfd, port, clientlen;
+	int listenfd, connfd, port, key;	
+	socklen_t client_length;
 	struct sockaddr_in clientaddr;
+	struct sockaddr_in serveraddr;
 	struct hostent *hp;
 	char *haddrp;
 	unsigned short client_port;
 
-	port = atoi(argv[1]); /* the server listens on a port passed on the command line */
+	if(argc != 3) {
+		fprintf(stderr, "usage: %s <port> <key>\n", argv[0]);
+		exit(0);	
+	}
 
-	listenfd = open_listenfd(port);
+	port = atoi(argv[1]); /* the server listens on a port passed on the command line */
+	key = atoi(argv[2]);
+
+	if((listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		printf(stderr, "Nope. No socket obtained.\n");
+		exit(-1);
+	}
+//	listenfd = open_listenfd(port);
+
+	memset(&severaddr, 0, sizeof(serveraddr));
+	//ALL THE SINS
+	serveraddr.sin_family = AF_INET;	
+	serveraddr.sin_addr.s_addr = htonl(INADDDR_ANY);
+	serveraddr.sin_port = htons(port);
+
+	if(bind(listenfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
+		printf(stderr, ("Listening(...) Call failed. Hanging up now.\n");
+		exit(-4);
+	}
+	
+	init_storage();
 
 	// Begin the WHILE loop here
 	clientlen = sizeof(clientaddr);
